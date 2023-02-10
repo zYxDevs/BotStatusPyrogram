79cdee71b4729d1f2f929e72eded8382f592ac41
Yoga Pranata <zyxdevs@otakufanshare.eu.org>
2023-02-10 20:32:05 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,73 +1,26 @@
-# Copyright ©️ 2021 TeLe TiPs. All Rights Reserved
-# You are free to use this code in any of your project, but you MUST include the following in your README.md (Copy & paste)
-# ##Credits - [BotStatus Telegram bot by TeLe TiPs] (https://github.com/teletips/Powerful_BotStatus-TeLeTiPs)
-
-# Changing the code is not allowed! Read GNU AFFERO GENERAL PUBLIC LICENSE: https://github.com/teletips/Powerful_BotStatus-TeLeTiPs/blob/main/LICENSE
-
-from pyrogram import Client, filters
+from asyncio import sleep
+from pyrogram import Client
 from pyrogram.errors import FloodWait
-import asyncio
-import datetime
-import pytz
-import os
+from config import API_HASH, API_ID, SUPPORT_ID, SESSION_NAME
 
 app = Client(
-    name="botstatus_teletips",
-    api_id=int(os.environ["API_ID"]),
-    api_hash=os.environ["API_HASH"],
-    session_string=os.environ["SESSION_STRING"],
+    name="restartub",
+    session_string=SESSION_NAME,
+    api_id=API_ID,
+    api_hash=API_HASH,
 )
-TIME_ZONE = os.environ["TIME_ZONE"]
-TIME_FORMAT = os.environ["TIME_FORMAT"]
-BOT_LIST = [i.strip() for i in os.environ.get("BOT_LIST").split(" ")]
-CHANNEL_OR_GROUP_ID = int(os.environ["CHANNEL_OR_GROUP_ID"])
-MESSAGE_ID = int(os.environ["MESSAGE_ID"])
-BOT_ADMIN_IDS = [int(i.strip()) for i in os.environ.get("BOT_ADMIN_IDS").split(" ")]
 
 
-async def main_teletips():
+async def main():
     async with app:
         while True:
-            print("Checking...")
-            xxx_teletips = f"📈 | **zYxDevs Bot Status**\n\n"
-            for bot in BOT_LIST:
-                name = await app.get_chat(bot)
-                try:
-                    yyy_teletips = await app.send_message(bot, "/start")
-                    aaa = yyy_teletips.id
-                    await asyncio.sleep(10)
-                    zzz_teletips = app.get_chat_history(bot, limit=1)
-                    async for ccc in zzz_teletips:
-                        bbb = ccc.id
-                    if aaa == bbb:
-                        xxx_teletips += f"• ❌ [{name.first_name}](https://t.me/{bot})\n"
-                        for bot_admin_id in BOT_ADMIN_IDS:
-                            try:
-                                await app.send_message(
-                                    int(bot_admin_id),
-                                    f"🚨 **@{bot} is down.**",
-                                )
-                            except Exception:
-                                pass
-                        await app.read_chat_history(bot)
-                    else:
-                        xxx_teletips += f"• ✅ [{name.first_name}](https://t.me/{bot})\n"
-                        await app.read_chat_history(bot)
-                except FloodWait as e:
-                    await asyncio.sleep(e.x)
-            time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
-            last_update = time.strftime(f"{TIME_FORMAT}")
-            xxx_teletips += f"\n📶 **Last Checked:** {last_update} - {TIME_ZONE}\n\n⚡️ **Powered by @Yoga_CIC**"
-            print(f"Last Checked on: {last_update}")
-            return await app.edit_message_text(
-                int(CHANNEL_OR_GROUP_ID),
-                MESSAGE_ID,
-                xxx_teletips,
-                disable_web_page_preview=True,
-            )
-            # await asyncio.sleep(6300)
+            print("Starting!")
+            try:
+                await app.send_message(SUPPORT_ID, "/restart")
+                await sleep(10)
+                await app.send_message(SUPPORT_ID, "/remdump")
+            except FloodWait as e:
+                await sleep(e.x)
+            return print("Complete!")
 
-
-app.run(main_teletips())
-
-# Copyright ©️ 2021 TeLe TiPs. All Rights Reserved
+app.run(main())
